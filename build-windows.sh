1440a8657aa7b159d0360b780923fdc992987db5
Bryan Phelps <bryphe@outlook.com>
2018-10-02 12:38:22 -0700

    Copy cmake binaries / share to common folder

--- build-windows.sh
+++ build-windows.sh
@@ -7,3 +7,13 @@ else
     echo "cygwin root: $ROOT"
     # /setup-x86_64.exe --root $ROOT -q --packages=cmake
 fi
+
+
+CMAKE_FOLDER="$(find /usr/share -maxdepth 1 -name cmake-*)"
+CMAKE_DIRNAME="$(basename $CMAKE_FOLDER)"
+
+echo "folder: $CMAKE_FOLDER"
+echo "dirname: $CMAKE_DIRNAME"
+
+cp /usr/bin/cmake.exe $cur__bin/cmake.exe
+cp -r $CMAKE_FOLDER $cur__share/$CMAKE_DIRNAME
