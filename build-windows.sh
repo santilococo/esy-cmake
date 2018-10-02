8e0b54b4613c6feb5d93cded01434a2e2ee45256
Bryan Phelps <bryphe@outlook.com>
2018-10-02 14:50:14 -0700

    Uninstall afterwards..

--- build-windows.sh
+++ build-windows.sh
@@ -6,14 +6,15 @@ else
     ROOT="$(cygpath -m /)"
     echo "cygwin root: $ROOT"
     /setup-x86_64.exe --root $ROOT -q --packages=cmake
+
+    CMAKE_FOLDER="$(find /usr/share -maxdepth 1 -name cmake-*)"
+    CMAKE_DIRNAME="$(basename $CMAKE_FOLDER)"
+
+    echo "folder: $CMAKE_FOLDER"
+    echo "dirname: $CMAKE_DIRNAME"
+
+    cp /usr/bin/cmake.exe $cur__bin/cmake.exe
+    cp -r $CMAKE_FOLDER $cur__share/$CMAKE_DIRNAME
+
+    /setup-x86_64.exe --root $ROOT -q --remove-packages=cmake
 fi
-
-
-CMAKE_FOLDER="$(find /usr/share -maxdepth 1 -name cmake-*)"
-CMAKE_DIRNAME="$(basename $CMAKE_FOLDER)"
-
-echo "folder: $CMAKE_FOLDER"
-echo "dirname: $CMAKE_DIRNAME"
-
-cp /usr/bin/cmake.exe $cur__bin/cmake.exe
-cp -r $CMAKE_FOLDER $cur__share/$CMAKE_DIRNAME
