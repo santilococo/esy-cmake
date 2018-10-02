91dbe9483a5c529b92b15dfc730b790634bf3549
Bryan Phelps <bryphe@outlook.com>
2018-10-01 19:36:50 -0700

    Set up windows hack

---
+++ build.sh
@@ -0,0 +1,9 @@
+BUILDDIR=$cur__root/_build
+PREFIX=$cur__install
+
+mkdir -p $(BUILDDIR)
+git clone https://github.com/kitware/cmake $(BUILDDIR)/cmake
+cd $(BUILDDIR)/cmake; git checkout c4ab098
+cd $(BUILDDIR)/cmake; ./bootstrap --prefix=$(PREFIX)
+cd $(BUILDDIR)/cmake; make
+cd $(BUILDDIR)/cmake; make install
