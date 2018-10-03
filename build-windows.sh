5a40a8eeb0f570cee28ac9eab8713b9465925ff3
Bryan Phelps <bryphe@outlook.com>
2018-10-02 17:05:55 -0700

    Fix cmake on Windows (#5)
    * Copy cmake binaries / share to common folder
    
    * Bring back running setup on windows
    
    * Uninstall afterwards..
    
    * Test latest
    
    * Add cmake

--- build-windows.sh
+++ build-windows.sh
@@ -1,9 +1,20 @@
-
-if cmake --version; then
-    echo "cmake already in path, not installing."
+if cmake.exe --version; then
+    echo "cmake already in path..."
 else
     echo "cmake not available, installing."
     ROOT="$(cygpath -m /)"
     echo "cygwin root: $ROOT"
-    # /setup-x86_64.exe --root $ROOT -q --packages=cmake
+    /setup-x86_64.exe --root $ROOT -q --packages=cmake
+
+    CMAKE_FOLDER="$(find /usr/share -maxdepth 1 -name cmake-*)"
+    CMAKE_DIRNAME="$(basename $CMAKE_FOLDER)"
+
+    echo "folder: $CMAKE_FOLDER"
+    echo "dirname: $CMAKE_DIRNAME"
+
+    echo "Copying to $cur__bin/cmake.exe"
+    cp /usr/bin/cmake.exe $cur__bin/cmake.exe
+
+    echo "Copying to $cur__share/$CMAKE_DIRNAME"
+    cp -r $CMAKE_FOLDER $cur__share/$CMAKE_DIRNAME
 fi
