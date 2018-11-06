37a86af82f7d93a784e4afcd220fb2bdc6b75fa9
Bryan Phelps <bryphe@outlook.com>
2018-11-05 18:39:42 -0800

    Don't bother building if cmake is already in path (#7)

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 BUILDDIR=$cur__root/_build
 PREFIX=$cur__install
 
-echo Using builddir: $BUILDDIR
-echo Using prefix: $PREFIX
+if which cmake; then
+    echo "Cmake already available, not building"
+else
+    echo Using builddir: $BUILDDIR
+    echo Using prefix: $PREFIX
 
-mkdir -p $BUILDDIR
-git clone https://github.com/kitware/cmake $BUILDDIR/cmake
-cd $BUILDDIR/cmake; git checkout c4ab098
-cd $BUILDDIR/cmake; ./bootstrap --prefix=$PREFIX
-cd $BUILDDIR/cmake; make
-cd $BUILDDIR/cmake; make install
+    mkdir -p $BUILDDIR
+    git clone https://github.com/kitware/cmake $BUILDDIR/cmake
+    cd $BUILDDIR/cmake; git checkout c4ab098
+    cd $BUILDDIR/cmake; ./bootstrap --prefix=$PREFIX
+    cd $BUILDDIR/cmake; make
+    cd $BUILDDIR/cmake; make install
+fi
