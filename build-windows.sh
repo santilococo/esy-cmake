69335b9264d94de460f525808f9305fc0c774960
Bryan Phelps <bryphe@outlook.com>
2020-08-12 12:45:07 -0700

    Don't copy binaries - just install

--- build-windows.sh
+++ build-windows.sh
@@ -11,13 +11,13 @@ else
     CMAKE_FOLDER="$(find /usr/share -maxdepth 1 -name cmake-*)"
     CMAKE_DIRNAME="$(basename $CMAKE_FOLDER)"
 
-    echo "folder: $CMAKE_FOLDER"
-    echo "dirname: $CMAKE_DIRNAME"
-
-    echo "Copying to $cur__bin/cmake.exe"
-    cp /usr/bin/cmake.exe $cur__bin/cmake.exe
-    cp /usr/bin/cygarchive-13.dll $cur__bin/cygarchive-13.dll
-
-    echo "Copying to $cur__share/$CMAKE_DIRNAME"
-    cp -r $CMAKE_FOLDER $cur__share/$CMAKE_DIRNAME
+#    echo "folder: $CMAKE_FOLDER"
+#    echo "dirname: $CMAKE_DIRNAME"
+#
+#    echo "Copying to $cur__bin/cmake.exe"
+#    cp /usr/bin/cmake.exe $cur__bin/cmake.exe
+#    cp /usr/bin/cygarchive-13.dll $cur__bin/cygarchive-13.dll
+#
+#    echo "Copying to $cur__share/$CMAKE_DIRNAME"
+#    cp -r $CMAKE_FOLDER $cur__share/$CMAKE_DIRNAME
 fi
