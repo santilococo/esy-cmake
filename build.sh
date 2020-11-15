c82a0c11b3e4e8bec7a676a1101303505f437b1f
Bryan Phelps <bryphe@outrunlabs.com>
2020-11-15 11:55:20 -0800

    Revert "Builds using 4 threads"
    This reverts commit 2ac1af6ea7a42fc5d675b39722c6d0c51f21fef7.

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ echo Using prefix: $PREFIX
 mkdir -p $BUILDDIR
 git clone https://github.com/kitware/cmake $BUILDDIR/cmake
 cd $BUILDDIR/cmake; git checkout 7700df9b1ef66761cad08cfc08344d5b27660e9f
-cd $BUILDDIR/cmake; ./bootstrap --prefix=$PREFIX --parallel=4
-cd $BUILDDIR/cmake; make -j4
+cd $BUILDDIR/cmake; ./bootstrap --prefix=$PREFIX
+cd $BUILDDIR/cmake; make
 cd $BUILDDIR/cmake; make install
 
