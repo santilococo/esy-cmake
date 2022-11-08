3b4b2c08f413fef6b04cd093a0bd58ff51a680fc
Santiago Lo Coco <santilococo.01@gmail.com>
2022-11-08 14:25:42 -0300

    Fix more bugs

--- build-windows.sh
+++ build-windows.sh
@@ -4,11 +4,7 @@ else
     echo "cmake not available, installing."
     c://ProgramData/Chocolatey/bin/choco.exe install cmake --installargs 'ADD_CMAKE_TO_PATH=User'
 
-    CMAKE_FOLDER="C:\ProgramData\chocolatey\lib\cmake"
-    ls -al $CMAKE_FOLDER
-    ls -al "C:\Program Files\CMake\bin"
     CMAKE_FOLDER="C:\Program Files\CMake\bin"
-    ls -al "C:\ProgramFiles\CMake\bin"
     CMAKE_DIRNAME="$(basename $CMAKE_FOLDER)"
 
     echo "folder: $CMAKE_FOLDER"
@@ -16,8 +12,7 @@ else
 
     echo "Copying to $cur__bin/cmake.exe"
     cp "$CMAKE_FOLDER"/cmake.exe $cur__bin/cmake.exe
-    cp /usr/bin/cygarchive-13.dll $cur__bin/cygarchive-13.dll
 
-    echo "Copying to $cur__share/$CMAKE_DIRNAME"
-    cp -r $CMAKE_FOLDER $cur__share/$CMAKE_DIRNAME
+    echo "Copying to $cur__share/cmake"
+    cp -r $CMAKE_FOLDER $cur__share/cmake
 fi
