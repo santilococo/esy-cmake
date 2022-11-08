191bff8d3d280486d296558c0d2e97618beafbf2
Santiago Lo Coco <santilococo.01@gmail.com>
2022-11-08 11:12:32 -0300

    Update build-windows.sh

--- build-windows.sh
+++ build-windows.sh
@@ -2,20 +2,17 @@ if cmake.exe --version; then
     echo "cmake already in path..."
 else
     echo "cmake not available, installing."
-    echo "$PATH"
-    echo "$(find /usr/bin -maxdepth 1 -name choco*)"
-    ls "c://ProgramData/Chocolatey"
-    ls "c://ProgramData/Chocolatey/bin"
     c://ProgramData/Chocolatey/bin/choco.exe install cmake --installargs 'ADD_CMAKE_TO_PATH=User'
 
-    CMAKE_FOLDER="$(find /usr/share -maxdepth 1 -name cmake-*)"
+    CMAKE_FOLDER="C:\ProgramData\chocolatey\lib\cmake"
+    ls -al $CMAKE_FOLDER
     CMAKE_DIRNAME="$(basename $CMAKE_FOLDER)"
 
     echo "folder: $CMAKE_FOLDER"
     echo "dirname: $CMAKE_DIRNAME"
 
     echo "Copying to $cur__bin/cmake.exe"
-    cp /usr/bin/cmake.exe $cur__bin/cmake.exe
+    cp "$CMAKE_FOLDER"/cmake.exe $cur__bin/cmake.exe
     cp /usr/bin/cygarchive-13.dll $cur__bin/cygarchive-13.dll
 
     echo "Copying to $cur__share/$CMAKE_DIRNAME"
